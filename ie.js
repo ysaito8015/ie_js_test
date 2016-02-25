68406e93d226935630e70962f58fd133c2efc50e
Yusuke Saito <ysaito8015@gmail.com>
2016-02-25 21:49:39 +0900

    amend ie.js

--- ie.js
+++ ie.js
@@ -1,27 +1,23 @@
+var ws =WScript.CreateObject("WScript.Shell");
 var ie = WScript.CreateObject("InternetExplorer.Application");
-  // IE起動
-  ie.Visible = true;
-  ie.Navigate( 'https://www.google.co.jp/' );
-  while ( (ie.busy) || (ie.redystate != 4) ) {
-    WScript.Sleep(100);
-  }
-  //waitIE( ie );
-  // 検索キーワードを入力
-  ie.document.getElementById("q").value = "test";
-  WScript.Sleep( 1000 );
-  // 検索ボタンクリック
-  ie.document.all("btnG").click();
-  WScript.Sleep( 1000 );
-  //waitIE( ie );
-  // 制御を破棄
-  ie.Quit();
-  ie = null;
+ie.Navigate( "http://www.yahoo.co.jp" );
+ie.Visible = true;
+while ( (ie.busy) || (ie.readystate != 4) ) {
+  WScript.Sleep(1000);
+}
+// 検索キーワードを入力
+//ie.document.all.Item("q").Value = 'Javascript';
+var srchtxt = ie.document.all("srchtxt");
+srchtxt.focus();
+srchtxt.Value = "Javascript";
+WScript.echo(srchtxt.Value);
+//WScript.echo(ie.document.all("p").length);
+WScript.Sleep( 1000 );
+// 検索ボタンクリック
+ie.document.all("srchbtn").click();
+WScript.Sleep( 1000 );
+// 制御を破棄
+ie.Quit();
+ie = null;
 
 
-// IEがビジー状態の間待ちます
-//function waitIE( ie ) {
-//  while( ( ie.Busy ) || ( ie.readystate != 4 ) ) {
-//    WScript.Sleep( 100 );
-//  }
-//  WScript.Sleep( 1000 )
-//}
